a92a67ffd2fff1b7a5782dc4d5dd524a2ad42722
Lahr Max <52128659+Lahma712@users.noreply.github.com>
2021-01-27 19:03:11 +0100

    Update MandelBrotV2.py
    added features like saving, adding iterations, randomizing color gradient

--- MandelBrotV2.py
+++ MandelBrotV2.py
@@ -4,8 +4,10 @@ from kivy.uix.widget import Widget
 from kivy.uix.image import Image as Bg
 from kivy.core.image import Image as CoreImage
 from io import BytesIO
+from kivy.uix.textinput import TextInput
 from PIL import Image, ImageDraw
 from kivy.core.window import Window
+from kivy.uix.button import Button
 from kivy.graphics import Line, InstructionGroup, Color
 import math
 import numpy as np
@@ -13,74 +15,79 @@ import numba as nb
 from kivy.clock import Clock
 from functions import DrawSet
 import time
+from gradient import polylinear_gradient
 kivy.require("2.0.0")
 
+
 class Draw(Widget):
-	Width = 500
-	Height =500
+	Width = 400
+	Height =400
 	ratio = float(Height/Width)
-	glow = 0
-	offset = 10
-	maxIt = 300
-	light = 255
+	maxIt = 20
 	WWidth = Width
 	WHeight = Height
 	Window.size = (WWidth, WHeight)
-	check = 1
-	xStart = -2
-	xEnd = 2
-	xDist = abs(xEnd-xStart)
+	xStart = -2.5
+	xEnd = 1.5
+	xDist = xEnd-xStart
 	yStart = -(xDist*ratio)/2
 	yEnd = (xDist*ratio)/2
-	yDist = abs(yEnd-yStart)
+	yDist = yEnd-yStart
 	array = np.zeros((Height, Width, 3), dtype=np.uint8)
-
+	colorPoints = 3
+	totalColors = 30
+	gradient = np.concatenate((np.array(polylinear_gradient(colorPoints, totalColors)), [(0,0,0)]), axis= 0)
+	check = 0
+	
+	print(len(gradient))
 
 	def __init__(self, **kwargs):
 		super(Draw, self).__init__(**kwargs)
 		with self.canvas:
-
 			self.Zoom = Window.request_keyboard(None, self)
 			self.Zoom.bind(on_key_up = self.ZoomInOut)
 			
-			self.MainSet = Bg(pos=(0, self.WHeight - self.Height), size= (self.Width, self.Height))
+			self.MainSet = Bg(pos=(0, 0), size= (self.Width, self.Height))
 			
 			self.bytes_io = BytesIO()
-			self.array = DrawSet(self.Width, self.Height, self.xStart, self.xDist , self.yStart, self.yDist, self.maxIt)
+			self.array = DrawSet(self.Width, self.Height, self.xStart, self.xDist , self.yStart, self.yDist, self.maxIt, self.gradient)
 			
-			self.img = Image.fromarray(np.flipud(self.array), 'HSV')
-			self.img.convert('RGB').save(self.bytes_io, 'PNG')
+			self.img = Image.fromarray(np.flipud(self.array), 'RGB')
+			self.img.save(self.bytes_io, 'PNG')
 			self.MainSet.texture = self.ImageByte(self, self.bytes_io.getvalue()).texture
 	
 			
 	def on_touch_move(self, touch):
+
 		self.secondtouch = touch.pos
-		self.Array = DrawSet(self.Width, self.Height, self.xStart, self.xDist, self.yStart, self.yDist, self.maxIt)
+		self.array = DrawSet(self.Width, self.Height, self.xStart, self.xDist, self.yStart, self.yDist, self.maxIt, self.gradient)
 			
 		self.bytes_io = BytesIO()
-		self.img = Image.fromarray(np.flipud(self.Array), 'HSV')
-		self.img.convert('RGB').save(self.bytes_io, 'PNG')
+		self.img = Image.fromarray(np.flipud(self.array), 'RGB')
+		self.img.save(self.bytes_io, 'PNG')
 		self.MainSet.texture = self.ImageByte(self, self.bytes_io.getvalue()).texture
 		
-		self.touch2 = [(self.secondtouch[0]/self.Width)*self.xDist + self.xStart, self.secondtouch[1]/self.Height*self.yDist + self.yStart]
-		self.touch1 = [(self.firsttouch[0]/self.Width)*self.xDist + self.xStart, self.firsttouch[1]/self.Height*self.yDist + self.yStart]
+		self.touch2 = [(self.secondtouch[0]/self.Width)*self.xDist + self.xStart, (self.secondtouch[1]/self.Height)*self.yDist + self.yStart]
+		self.touch1 = [(self.firsttouch[0]/self.Width)*self.xDist + self.xStart, (self.firsttouch[1]/self.Height)*self.yDist + self.yStart]
 		self.shift = [self.touch2[0]-self.touch1[0], self.touch2[1]-self.touch1[1]]	
-		self.xStart = self.xStart - self.shift[0]
-		self.xEnd = self.xEnd - self.shift[0]
+		self.xStart = (self.xStart - self.shift[0])
+		self.xEnd = (self.xEnd - self.shift[0])
 
-		self.yStart = self.yStart - self.shift[1]
-		self.yEnd = self.yEnd - self.shift[1]
+		self.yStart = (self.yStart - self.shift[1])
+		self.yEnd = (self.yEnd - self.shift[1])
 
-		self.xDist = abs(self.xEnd - self.xStart)
-		self.yDist = abs(self.yEnd - self.yStart)
+		self.xDist = (self.xEnd - self.xStart)
+		self.yDist = (self.yEnd - self.yStart)
 		
 		self.firsttouch = self.secondtouch
 				
 	def on_touch_down(self, touch):
 		self.firsttouch = touch.pos
+		if touch.pos[1] > self.Height:
+			super(Draw, self).on_touch_down(touch)
 
 	def ZoomInOut(self, window, keycode):
-		
+		start = time.time()
 		if keycode[1] == "w":
 			self.xStart = self.xStart + self.xDist*0.20
 			self.xEnd = self.xEnd - self.xDist*0.20
@@ -88,8 +95,8 @@ class Draw(Widget):
 			self.yStart = self.yStart + self.yDist*0.20
 			self.yEnd = self.yEnd - self.yDist*0.20
 
-			self.xDist = abs(self.xEnd - self.xStart)
-			self.yDist = abs(self.yEnd - self.yStart)
+			self.xDist = self.xEnd - self.xStart
+			self.yDist = self.yEnd - self.yStart
 
 	
 		elif keycode[1] == "s":
@@ -98,17 +105,85 @@ class Draw(Widget):
 			self.yStart = self.yStart - self.yDist*0.20
 			self.yEnd = self.yEnd + self.yDist*0.20
 
-			self.xDist = abs(self.xEnd - self.xStart)
-			self.yDist = abs(self.yEnd - self.yStart)
+			self.xDist = self.xEnd - self.xStart
+			self.yDist = self.yEnd - self.yStart
 
-		self.Array = DrawSet(self.Width, self.Height, self.xStart, self.xDist, self.yStart, self.yDist, self.maxIt)
-		self.bytes_io = BytesIO()
-		self.img = Image.fromarray(np.flipud(self.Array), 'HSV')
-		self.img.convert('RGB').save(self.bytes_io, 'PNG')
+		elif keycode[1] == "up":
+			self.maxIt += 20
+		elif keycode[1] == "down":
+			self.maxIt -= 20
+		elif keycode[1] == "r":
+			self.gradient = np.concatenate((np.array(polylinear_gradient(self.colorPoints, self.totalColors)), [(0,0,0)]), axis= 0)
 
-		self.MainSet.texture = self.ImageByte(self, self.bytes_io.getvalue()).texture
+		elif keycode[1] == "f":
+			with self.canvas:
+
+				if self.check == 0:
+					self.WidthBox = TextInput(text = "Width: ", font_size = self.Height * 0.022, size = (self.Width * 0.25, self.Height*0.05), pos = (0,self.Height), multiline = False)
+					self.HeightBox = TextInput(text = "Height: ", font_size = self.Height * 0.022, size = (self.Width * 0.25, self.Height*0.05), pos = (self.Width * 0.25,self.Height), multiline = False)
+					self.AntialiasBox = TextInput(text = "Antialias: ", font_size = self.Height * 0.022, size = (self.Width * 0.25, self.Height*0.05), pos = (self.Width * 0.50, self.Height), multiline = False)
+					self.SaveBtn = Button(text="Save", font_size=self.Height*0.03, size = (self.Width *0.25, self.Height*0.05), pos =(self.Width * 0.75, self.Height))
+					self.add_widget(self.SaveBtn)
+					self.add_widget(self.AntialiasBox)
+					self.add_widget(self.HeightBox)
+					self.add_widget(self.WidthBox)
+					self.SaveBtn.bind(on_press = self.Save)
+					self.WidthBox.bind(text = self.WidthText)
+					self.HeightBox.bind(text = self.HeightText)
+					self.AntialiasBox.bind(text = self.AntialiasText)
+					Window.size = (self.Width, self.Height + self.WidthBox.size[1])
+					self.check = 1
+
+				else:
+
+					self.remove_widget(self.WidthBox)
+					self.remove_widget(self.HeightBox)
+					self.remove_widget(self.AntialiasBox)
+					self.remove_widget(self.SaveBtn)
+					Window.size = (self.Width, self.Height)
+					self.check = 0
+				
+
 			
+	
+
+		self.array = DrawSet(self.Width, self.Height, self.xStart, self.xDist, self.yStart, self.yDist, self.maxIt, self.gradient)
+		self.bytes_io = BytesIO()
+		self.img = Image.fromarray(np.flipud(self.array), 'RGB')
+		self.img.save(self.bytes_io, 'PNG')
+		self.MainSet.texture = self.ImageByte(self, self.bytes_io.getvalue()).texture
+
 		
+	
+	def WidthText(self, instance, text):
+		try:
+			self.SWidth = int(''.join(filter(str.isdigit, self.WidthBox.text)))
+		except:
+			pass
+
+	def HeightText(self, instance, text):
+		try:
+			self.SHeight = int(''.join(filter(str.isdigit, self.HeightBox.text)))
+		except:
+			pass
+
+	def AntialiasText(self, instance, text):
+		try: 
+			self.Antialias = int(''.join(filter(str.isdigit, self.AntialiasBox.text)))
+		except:
+			pass
+	
+		
+	def Save(self, instance):
+		try:
+			self.array = DrawSet(self.SWidth*self.Antialias, self.SHeight*self.Antialias, self.xStart, self.xDist, (self.yStart + (self.xDist*self.ratio)/2) - (self.xDist * (self.SHeight/self.SWidth))/2, self.xDist * (self.SHeight/self.SWidth), self.maxIt, self.gradient)
+			self.img = Image.fromarray(np.flipud(self.array), 'RGB')
+			self.img = self.img.resize((self.SWidth, self.SHeight), Image.ANTIALIAS)
+			self.img.save('Fractal.png')
+			self.img.show()
+		except:
+			pass
+
 
 	def ImageByte(self, instance, ImageByte):
 		self.Buffer = BytesIO(ImageByte)
