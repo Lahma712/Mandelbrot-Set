fac35791d05f4ef0560a68cc78f6000d34e0c332
Lahr Max <52128659+Lahma712@users.noreply.github.com>
2021-02-04 15:55:41 +0100

    Update ColorPicker.py

--- ColorPicker.py
+++ ColorPicker.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def loop(index, HStart, HEnd, H, W, array, colorBar, op): #loop that is used to create rainbow colorbar
 	division = 255/(H/6) #colorbar can be divided into 6 smaller units where in each unit, one RGB value goes up/down by 255 
 	remainder = division % 1 
